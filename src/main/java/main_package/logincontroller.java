7200f986c8c3cc2f45776f6247c92b41aa8991f4
mohammeddyasserr <mohammedyasser.info@gmail.com>
2025-04-24 16:44:38 +0200

    Initial commit

---
+++ src/main/java/main_package/logincontroller.java
@@ -0,0 +1,103 @@
+package main_package;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class logincontroller {
+
+    @FXML
+    private ImageView logo;
+    @FXML
+    private RadioButton admin;
+    @FXML
+    private RadioButton user;
+    @FXML
+    private ToggleGroup user_admin;
+    @FXML
+    private TextField userfield;
+    @FXML
+    private PasswordField passfield;
+    @FXML
+    private Button signinbtn;
+    @FXML
+    private Label question;
+    @FXML
+    private Button signup;
+    @FXML
+    private TextField pass2signup;
+    @FXML
+    private TextField usernamesignup;
+    @FXML
+    private TextField pass1signup;
+    @FXML
+    private TextField phonesignup;
+    @FXML
+    private TextField addresssignup;
+    @FXML
+    private Button signupbtn;
+
+    @FXML
+    void adminselect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signinclick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signup(ActionEvent event) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(logo);  // تحديد الشعار كالعنصر الذي سيتم تحريكه
+        transition.setByX(-440);    // تحريك الشعار بمقدار 300 وحدة إلى اليمين (يمكنك تعديل هذه القيمة حسب احتياجك)
+        transition.setDuration(Duration.seconds(1)); // تحديد مدة الانتقال (1 ثانية في هذا المثال)
+        transition.play();  // تشغيل الانتقال
+        userfield.setVisible(true);
+        passfield.setVisible(true);
+        admin.setVisible(true);
+        user.setVisible(true);
+        signinbtn.setVisible(true);
+        question.setVisible(true);
+        signup.setVisible(true);
+        pass2signup.setVisible(false);
+        usernamesignup.setVisible(false);
+        pass1signup.setVisible(false);
+        phonesignup.setVisible(false);
+        addresssignup.setVisible(false);
+        signupbtn.setVisible(false);
+    }
+
+    @FXML
+    void signuprequest(ActionEvent event) {
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(logo);  // تحديد الشعار كالعنصر الذي سيتم تحريكه
+        transition.setByX(440);    // تحريك الشعار بمقدار 300 وحدة إلى اليمين (يمكنك تعديل هذه القيمة حسب احتياجك)
+        transition.setDuration(Duration.seconds(1)); // تحديد مدة الانتقال (1 ثانية في هذا المثال)
+        transition.play();  // تشغيل الانتقال
+        userfield.setVisible(false);
+        passfield.setVisible(false);
+        admin.setVisible(false);
+        user.setVisible(false);
+        signinbtn.setVisible(false);
+        question.setVisible(false);
+        signup.setVisible(false);
+        pass2signup.setVisible(true);
+        usernamesignup.setVisible(true);
+        pass1signup.setVisible(true);
+        phonesignup.setVisible(true);
+        addresssignup.setVisible(true);
+        signupbtn.setVisible(true);
+    }
+
+    @FXML
+    void userselect(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module team.ohm_zone {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens main_package to javafx.fxml;
+    exports main_package;
+}
