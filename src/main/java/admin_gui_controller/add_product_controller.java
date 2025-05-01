cd0a27195370d2e111264a6a63bce3ad5c2d7b3b
mohammeddyasserr <mohammedyasser.info@gmail.com>
2025-05-01 12:51:59 +0300

    linking add_product

--- src/main/java/admin_gui_controller/add_product_controller.java
+++ src/main/java/admin_gui_controller/add_product_controller.java
@@ -3,12 +3,19 @@ package admin_gui_controller;
 import db_edit_functions.product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import main_package.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -74,6 +81,74 @@ public class add_product_controller implements Initializable {
 
     @FXML
     private Pane side_panel;
+
+    @FXML
+    void acount_page(ActionEvent event) {
+
+    }
+
+    @FXML
+    void adda_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/add_admin.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void addp_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/add_product.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void editp_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/edit_product.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void home_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/admin_main.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void pills_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/pills_admin.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void removep_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/remove_product.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void login_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main_package/login.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     void toggle_menu(ActionEvent event) {
