b174e620d914b145071e58793e812373ec20f981
mohammeddyasserr <mohammedyasser.info@gmail.com>
2025-05-01 12:21:25 +0300

    edit tables

---
+++ src/main/java/admin_gui_controller/add_admin_controller.java
@@ -0,0 +1,23 @@
+package admin_gui_controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class add_admin_controller {
+
+    @FXML
+    private Pane side_panel;
+
+    @FXML
+    private Button menu_btn;
+
+    @FXML
+    void toggle_menu(ActionEvent event) {
+        boolean isVisible=side_panel.isVisible();
+        side_panel.setVisible(!isVisible);
+        //side_panel.setManaged(!isVisible);
+    }
+
+}
