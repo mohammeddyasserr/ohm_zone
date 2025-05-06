42ad6085112c2f43272f2398ab3cd9ce8a5d98cf
abolmagd1 <youssefabolmagd0@gmail.com>
2025-05-06 23:18:22 +0300

    user_acc_page . 1

---
+++ src/main/java/user_gui_controller/user_account_controller.java
@@ -0,0 +1,133 @@
+package user_gui_controller;
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.Pane;
+//import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import javafx.event.ActionEvent;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+
+
+public class user_account_controller {
+
+
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private AnchorPane left_menu;
+
+    @FXML
+    private ScrollPane main_scroll_pane;
+
+    private boolean isMenuVisible = true;
+
+
+
+
+    @FXML
+    private void toggle_menu() {
+        isMenuVisible = !isMenuVisible;
+
+        if (isMenuVisible) {
+            // Show menu with animation
+            left_menu.setVisible(true);
+            left_menu.setManaged(true);
+
+            TranslateTransition slideIn = new TranslateTransition(Duration.millis(300), left_menu);
+            slideIn.setFromX(-left_menu.getWidth());
+            slideIn.setToX(0);
+            slideIn.play();
+
+            AnchorPane.setLeftAnchor(main_scroll_pane, 165.0);
+        } else {
+            // Hide menu with animation
+            TranslateTransition slideOut = new TranslateTransition(Duration.millis(300), left_menu);
+            slideOut.setFromX(0);
+            slideOut.setToX(-left_menu.getWidth());
+            slideOut.setOnFinished(event -> {
+                left_menu.setVisible(false);
+                left_menu.setManaged(false);
+            });
+            slideOut.play();
+
+            AnchorPane.setLeftAnchor(main_scroll_pane, 0.0);
+        }
+    }
+
+    @FXML
+    void calculator_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/calc.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void acount_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/account_user.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void cart_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/cart.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void home_page_user(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/user_main.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void login_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main_package/login.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void pills_page_user(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/pills.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void shop_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/shop.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
