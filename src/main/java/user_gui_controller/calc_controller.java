9506e621bddc417297a62113c70a36a921fcba43
mohammeddyasserr <mohammedyasser.info@gmail.com>
2025-05-06 16:51:07 +0300

    centered logo fix calc_controller

--- src/main/java/user_gui_controller/calc_controller.java
+++ src/main/java/user_gui_controller/calc_controller.java
@@ -1,13 +1,22 @@
 package user_gui_controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -61,34 +70,69 @@ public class calc_controller {
         //side_panel.setManaged(!isVisible);
     }
 
-    @FXML
-    void calculator_page(ActionEvent event) {
 
+
+    @FXML
+    void calculator_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/calc.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void cart_page(ActionEvent event) {
-
+    void acount_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/account_user.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void home_page_user(ActionEvent event) {
-
+    void cart_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/cart.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void login_page(ActionEvent event) {
-
+    void home_page_user(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/user_main.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void pills_page_user(ActionEvent event) {
-
+    void login_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main_package/login.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void shop_page(ActionEvent event) {
+    void pills_page_user(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/admin_gui/pills.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    @FXML
+    void shop_page(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/user_gui/shop.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     private void setupMaps() {
